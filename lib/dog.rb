d34b59c0d812213b0f534eb65b70dbf74aa15de8
Machoka Joash Omao <aaronjoash44@gmail.com>
2022-10-20 03:02:59 +0300

    completed phase-3-ruby-oo-basics-setter-and-getter-methods

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,26 @@
 class Dog
+    # setter methods
+    def name=(name)
+        @name = name
+    end
+
+    def breed=(breed)
+        @breed = breed
+    end
+
+    # getter methods
+    def name
+        @name
+    end 
+
+    def breed
+        @breed
+    end
 
 end
+
+corgi = Dog.new
+corgi.name = "Rex"
+corgi.breed = "Corgi"
+puts corgi.name
+puts corgi.breed
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,26 @@
 class Person
+    # setter methods
+    def name=(name)
+        @name = name
+    end
+
+    def job=(job)
+        @job = job
+    end
+
+    # getter methods
+    def name
+        @name
+    end
+
+    def job
+        @job
+    end
 
 end
+
+p1 = Person.new
+p1.name = "Ellen White"
+p1.job = "Preacher"
+puts p1.name
+puts p1.job
